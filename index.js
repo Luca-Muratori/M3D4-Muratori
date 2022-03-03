55faa7481af73f64edb3d9f32345d89fe1227396
Lucai994 <luca.muratori.94@hotmail.it>
2022-03-03 15:44:59 +0100

    commit

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 //fetch the books data and display on the page
 
 const main = document.querySelector("#mainContainerBook");
+const shoppingArea = document.querySelector("#shoppingCart");
+shoppingArea.innerHTML = `
+      <h3 id='title'>Shopping Cart</h3>
+  `;
 
 const fetchBook = () => {
   const fetchData = fetch("https://striveschool-api.herokuapp.com/books")
     .then((response) => response.json())
     .then((response) => {
       console.log(response);
-
       response.forEach((book) => {
         const col = document.createElement("div");
         col.classList.add("col");
@@ -19,7 +22,8 @@ const fetchBook = () => {
                 <div class="card-body">
                     <h5 class="card-title">${book.title}</h5>
                     <p class="card-text">${book.price} $</p>
-                    <a href="#" onclick=addToCart() class="btn btn-primary">Add to cart</a>
+                    <a href="#" onclick=addToCart(event) class="btn btn-primary">Add to cart</a>
+                    <a href="#" onclick=skipBook(event) class="btn btn-danger">Skip this book</a>
                 </div>
             </div>
           `;
@@ -29,15 +33,19 @@ const fetchBook = () => {
     .catch((error) => console.log(error));
 };
 //create the cart and add ll the function
-
-const shoppingArea = document.querySelector("#shoppingCart");
-shoppingArea.innerHTML = `
-      <h3 id='title'>Shopping Cart</h3>
-  `;
 let bookArrCart = [];
-const addToCart = (book) => {
-  bookArrCart.push(book.asin);
+const addToCart = (e) => {
+  let book = e.target.closest(".card");
+  bookArrCart.push(book);
+  book.classList.add("selected");
+  //   bookArrCart.push(e);
   console.log(bookArrCart);
+};
+
+//skip the book, when clicked in the btn
+const skipBook = (e) => {
+  let col = e.target.closest(".col");
+  col.remove();
 };
 
 window.onload = () => {

86ed3a9fef253c67223525ca359399941f59c28b
Lucai994 <luca.muratori.94@hotmail.it>
2022-03-03 17:15:46 +0100

    commit

--- index.js
+++ index.js
@@ -1,53 +1,113 @@
-//fetch the books data and display on the page
-
-const main = document.querySelector("#mainContainerBook");
-const shoppingArea = document.querySelector("#shoppingCart");
-shoppingArea.innerHTML = `
-      <h3 id='title'>Shopping Cart</h3>
-  `;
-
-const fetchBook = () => {
-  const fetchData = fetch("https://striveschool-api.herokuapp.com/books")
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(response);
-      response.forEach((book) => {
-        const col = document.createElement("div");
-        col.classList.add("col");
-        col.classList.add("col-lg-3");
-        col.classList.add("col-md-4");
-        col.innerHTML = `
-            <div class="card" >
-                <img src=${book.img} class="card-img-top" alt="book-cover">
-                <div class="card-body">
-                    <h5 class="card-title">${book.title}</h5>
-                    <p class="card-text">${book.price} $</p>
-                    <a href="#" onclick=addToCart(event) class="btn btn-primary">Add to cart</a>
-                    <a href="#" onclick=skipBook(event) class="btn btn-danger">Skip this book</a>
-                </div>
-            </div>
-          `;
-        main.appendChild(col);
-      });
-    })
-    .catch((error) => console.log(error));
-};
-//create the cart and add ll the function
-let bookArrCart = [];
-const addToCart = (e) => {
-  let book = e.target.closest(".card");
-  bookArrCart.push(book);
-  book.classList.add("selected");
-  //   bookArrCart.push(e);
-  console.log(bookArrCart);
-};
-
-//skip the book, when clicked in the btn
-const skipBook = (e) => {
-  let col = e.target.closest(".col");
-  col.remove();
-};
+const booksWrapper = document.querySelector("#books-wrapper");
+const shoppingCart = document.querySelector("#shopping-cart");
 
 window.onload = () => {
-  fetchBook();
+  loadBooks();
 };
+
+let books = [];
+
+let shoppingCartList = [];
+
+let filteredBooks = [];
+
+function loadBooks() {
+  fetch("https://striveschool-api.herokuapp.com/books")
+    .then((r) => r.json())
+    .then((_books) => {
+      books = _books;
+      console.log(books);
+      displayBooks();
+    })
+    .catch((err) => console.error(err.message));
+}
+
+function displayBooks(_books = books) {
+  booksWrapper.innerHTML = "";
+
+  _books.forEach((book) => {
+    booksWrapper.innerHTML += `
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+              <div class="card">
+                <img src="${book.img}" class="img-fluid card-img-top" alt="${
+      book.title
+    }">
+                <div class="card-body">
+                  <h5 class="card-title">${book.title}</h5>
+                  <p class="card-text">${book.category}</p>
+                  <button class="btn btn-primary" onclick="addToCart('${String(
+                    book.asin
+                  )}', this)">$${book.price}</button>
+                  <button class="btn btn-warning" onclick="this.closest('.col-12').remove()">
+                    Skip me
+                  </button>
+                </div>
+              </div>
+            </div>
+          `;
+  });
+}
+
+function addToCart(asin, element) {
+  console.log(asin);
+  const book = books.filter((book) => book.asin === asin)[0];
+  shoppingCartList.push(book);
+  console.log(shoppingCartList);
+
+  refreshShoppingCart();
+
+  element.closest(".card").classList.add("selected");
+}
+
+function refreshShoppingCart() {
+  shoppingCart.innerHTML = "";
+
+  shoppingCartList.forEach((book) => {
+    shoppingCart.innerHTML += `
+    <div class='col-lg-2 col-md-3'>
+        <div class="shopping-item ">
+            <div>
+                ${book.title}
+            </div>
+            <div>
+                $${book.price}
+            </div>
+            <div class='footer-itemCart'>
+                <div>
+                    <button class="btn btn-danger" onclick="deleteItem('${String(
+                      book.asin
+                    )}')">Delete </button>
+                </div>
+                <img  class='img-cart' src="${book.img}"/>
+            </div>
+
+        </div>
+    </div>
+          `;
+  });
+}
+
+function search(query) {
+  if (query.length < 3) {
+    filteredBooks = books;
+    displayBooks();
+    return;
+  }
+
+  filteredBooks = books.filter((book) =>
+    book.title.toLowerCase().includes(query.toLowerCase())
+  );
+
+  console.log(filteredBooks);
+  displayBooks(filteredBooks);
+}
+
+function deleteItem(asin) {
+  const index = shoppingCartList.findIndex((book) => book.asin === asin);
+
+  if (index !== -1) {
+    shoppingCartList.splice(index, 1);
+  }
+
+  refreshShoppingCart();
+}
